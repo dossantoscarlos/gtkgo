2bb95a2b979217139af22eba5b293969d3380e7b
Cadu <anjoazrael10@gmail.com>
2024-12-24 03:12:34 -0300

    removendo parametro de retorno

--- core/adapters/controllers/auth_controller.go
+++ core/adapters/controllers/auth_controller.go
@@ -15,7 +15,7 @@ func NewAuthController() *AuthController {
 	return &AuthController{auth_service: auth_service}
 }
 
-func (auth *AuthController) Authenticate(name string, password string) (authDto *dto.AuthDTO, err error) {
+func (auth *AuthController) Authenticate(name string, password string) (*dto.AuthDTO, error) {
 
 	user, err := auth.auth_service.AuthenticateService(name, password)
 	if err != nil {
